8ac772162cd02ca96c2ca98332a2ca6ca3c0c25f
Sean Mulligan <sean.mulligan85@gmail.com>
2016-10-18 09:49:04 -0700

    Remove 'does something useful' spec

--- spec/heyupdate_spec.rb
+++ spec/heyupdate_spec.rb
@@ -4,8 +4,4 @@ describe Heyupdate do
   it 'has a version number' do
     expect(Heyupdate::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
